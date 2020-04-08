823931065ca6d4c48b39649d67da6c8fb4a7bb9e
Mads Ovesen <m.ovesen@allsaints.com>
2020-04-08 18:01:40 +0100

    refactored recusive tree

--- bstree/bstree-rec.go
+++ bstree/bstree-rec.go
@@ -20,14 +20,22 @@
 //				 2   4  13
 //				       /
 //				      9
+//
+// A binary search tree's structure is dependent on the order of the inserted
+// nodes, but a nonempty tree with x nodes must have a height of at least
+// ⌊lg x⌋ and at most x. The expected runtime of most of the basic operations is
+// equivalent to the height of the tree, therefore making it Θ(x). On average
+// a lower runtime of Θ(lg x) would be expected though, especially if the tree
+// is randomly built.
+// This package is split into two files with the same operations, except that
+// one set uses a recursive strategy, whereas the other set uses an iterative
+// strategy. Both sets can be used interchangeably. The recursive operations
+// tend to be more reable and requires less lines on average, but most iterative
+// operations are slightly more efficient in terms of memory amd runtime.
 package bstree
 
-import (
-	"fmt"
-)
-
-// TreeRec defines a tree structure using a recursive implementation strategy.
-type TreeRec struct {
+// Tree defines a tree structure using a recursive implementation strategy.
+type Tree struct {
 	root  *Node
 	count int
 }
@@ -40,9 +48,9 @@ type Node struct {
 	value  int
 }
 
-// NewBSTreeRec creates a new instance of a TreeRec.
-func NewBSTreeRec() *TreeRec {
-	return &TreeRec{}
+// NewBSTree creates a new instance of a Tree.
+func NewBSTree() *Tree {
+	return &Tree{}
 }
 
 // Insert adds a node n to the correct position in the tree, by following the
@@ -50,7 +58,7 @@ func NewBSTreeRec() *TreeRec {
 // parent of n is found. A simple value comparison is between n's value and the
 // parent is used to determine if n will be the left or right child of the
 // parent.
-func (t *TreeRec) Insert(val int) {
+func (t *Tree) Insert(val int) {
 	r := t.root
 	if r == nil {
 		t.root = &Node{nil, nil, nil, val}
@@ -69,7 +77,7 @@ func (t *TreeRec) Insert(val int) {
 
 // ins is a helper function that traces down the tree and returns the first node
 // with a nil child that satisfies the binary-search-tree property.
-func (t *TreeRec) ins(n *Node, val int) *Node {
+func (t *Tree) ins(n *Node, val int) *Node {
 	if val <= n.value {
 		if n.left != nil {
 			return t.ins(n.left, val)
@@ -90,7 +98,7 @@ func (t *TreeRec) ins(n *Node, val int) *Node {
 // If n has no children, n will just be deleted. Else if n has one child, the
 // child is copied to n, and the child is deleted. Else n must have two
 // children, and n must have a successor which is copied to n and then deleted.
-func (t *TreeRec) Delete(val int) {
+func (t *Tree) Delete(val int) {
 	r := t.root
 	if r == nil {
 		return
@@ -108,7 +116,7 @@ func (t *TreeRec) Delete(val int) {
 // del is a helper function that traces down the tree and finds the first node
 // with the given value. Depending on the number of children, the respective
 // operation is done.
-func (t *TreeRec) del(n *Node, val int) *Node {
+func (t *Tree) del(n *Node, val int) *Node {
 	if val < n.value {
 		n.left = t.del(n.left, val)
 		return n
@@ -140,7 +148,11 @@ func (t *TreeRec) del(n *Node, val int) *Node {
 // search value is greater than n's value it continues along n's right subtree.
 // The binary-search-tree property ensures that this is the correct, and that
 // if the search value exists, it will be found.
-func (t *TreeRec) Search(n *Node, val int) *Node {
+func (t *Tree) Search(n *Node, val int) *Node {
+	if n == nil {
+		n = t.root
+	}
+
 	if val == n.value {
 		return n
 	}
@@ -162,11 +174,13 @@ func (t *TreeRec) Search(n *Node, val int) *Node {
 
 // Min finds the node with the minimum value of a given node n's subtree, i.e.
 // the leftmost node in the subtree.
-func (t *TreeRec) Min(n *Node) *Node {
-	if n != nil {
-		if n.left != nil {
-			return t.Min(n.left)
-		}
+func (t *Tree) Min(n *Node) *Node {
+	if n == nil {
+		n = t.root
+	}
+
+	if n.left != nil {
+		return t.Min(n.left)
 	}
 
 	return n
@@ -174,11 +188,13 @@ func (t *TreeRec) Min(n *Node) *Node {
 
 // Max finds the node with the maximum value of a given node n's subtree, i.e.
 // the rightmost node in the substree.
-func (t *TreeRec) Max(n *Node) *Node {
-	if n != nil {
-		if n.right != nil {
-			return t.Max(n.right)
-		}
+func (t *Tree) Max(n *Node) *Node {
+	if n == nil {
+		n = t.root
+	}
+
+	if n.right != nil {
+		return t.Max(n.right)
 	}
 
 	return n
@@ -190,15 +206,17 @@ func (t *TreeRec) Max(n *Node) *Node {
 // If the right subtree of node n is empty, then either n does not have a
 // successor or the successor is the lowest ancestor of n whose left child is
 // also an ancestor of n.
-func (t *TreeRec) Successor(n *Node) *Node {
-	if n != nil {
-		if n.right != nil {
-			return t.Min(n.right)
-		}
+func (t *Tree) Successor(n *Node) *Node {
+	if n == nil {
+		n = t.root
+	}
 
-		if n.parent != nil {
-			return t.suc(n, n.parent)
-		}
+	if n.right != nil {
+		return t.Min(n.right)
+	}
+
+	if n.parent != nil {
+		return t.suc(n, n.parent)
 	}
 
 	return nil
@@ -210,15 +228,17 @@ func (t *TreeRec) Successor(n *Node) *Node {
 // If the left substree of node n is empty, then either n does not have a
 // predecessor or the predecessor is the lowest ancestor of n whose right child
 // is also an ancestor of n.
-func (t *TreeRec) Predecessor(n *Node) *Node {
-	if n != nil {
-		if n.left != nil {
-			return t.Max(n.left)
-		}
+func (t *Tree) Predecessor(n *Node) *Node {
+	if n == nil {
+		n = t.root
+	}
 
-		if n.parent != nil {
-			return t.pre(n, n.parent)
-		}
+	if n.left != nil {
+		return t.Max(n.left)
+	}
+
+	if n.parent != nil {
+		return t.pre(n, n.parent)
 	}
 
 	return nil
@@ -227,9 +247,13 @@ func (t *TreeRec) Predecessor(n *Node) *Node {
 // suc is a helper function that traces up the tree from a given node n, and
 // checks if n is the right child of its parent. When this is false, i.e. n is a
 // left child, the function returns n's parent.
-func (t *TreeRec) suc(n, par *Node) *Node {
-	if n.value == par.right.value {
-		return t.suc(n.parent, par.parent)
+func (t *Tree) suc(n, par *Node) *Node {
+	if par.right != nil && n.value == par.right.value {
+		if par.parent != nil {
+			return t.suc(n.parent, par.parent)
+		}
+
+		return nil
 	}
 
 	return par
@@ -238,9 +262,13 @@ func (t *TreeRec) suc(n, par *Node) *Node {
 // pre is a helper function that traces up the tree from a given node n, and
 // checks if n is the left child of its parent. When this is false, i.e. n is a
 // right child, the function returns n's parent.
-func (t *TreeRec) pre(m, par *Node) *Node {
-	if m.value == par.left.value {
-		return t.pre(m.parent, par.parent)
+func (t *Tree) pre(m, par *Node) *Node {
+	if par.left != nil && m.value == par.left.value {
+		if par.parent != nil {
+			return t.pre(m.parent, par.parent)
+		}
+
+		return nil
 	}
 
 	return par
@@ -248,51 +276,57 @@ func (t *TreeRec) pre(m, par *Node) *Node {
 
 // InOrder prints an inordered list of the values of node n's subtree.
 // This results in an ascending ordered list of the values.
-func (t *TreeRec) InOrder(n *Node) {
-	if n != nil {
-		if n.left != nil {
-			t.InOrder(n.left)
-		}
+func (t *Tree) InOrder(n *Node, f func(*Node)) {
+	if n == nil {
+		n = t.root
+	}
 
-		fmt.Printf("%d ", n.value)
+	if n.left != nil {
+		t.InOrder(n.left, f)
+	}
 
-		if n.right != nil {
-			t.InOrder(n.right)
-		}
+	f(n)
+
+	if n.right != nil {
+		t.InOrder(n.right, f)
 	}
 }
 
 // PreOrder prints a preordered list of the values of node n's subtree.
-func (t *TreeRec) PreOrder(n *Node) {
-	if n != nil {
-		fmt.Printf("%d ", n.value)
+func (t *Tree) PreOrder(n *Node, f func(*Node)) {
+	if n == nil {
+		n = t.root
+	}
 
-		if n.left != nil {
-			t.PreOrder(n.left)
-		}
+	f(n)
 
-		if n.right != nil {
-			t.PreOrder(n.right)
-		}
+	if n.left != nil {
+		t.PreOrder(n.left, f)
+	}
+
+	if n.right != nil {
+		t.PreOrder(n.right, f)
 	}
 }
 
 // PostOrder prints a postordered list of the values of node n's subtree.
-func (t *TreeRec) PostOrder(n *Node) {
-	if n != nil {
-		if n.left != nil {
-			t.PostOrder(n.left)
-		}
-
-		if n.right != nil {
-			t.PostOrder(n.right)
-		}
-
-		fmt.Printf("%d ", n.value)
+func (t *Tree) PostOrder(n *Node, f func(*Node)) {
+	if n == nil {
+		n = t.root
 	}
+
+	if n.left != nil {
+		t.PostOrder(n.left, f)
+	}
+
+	if n.right != nil {
+		t.PostOrder(n.right, f)
+	}
+
+	f(n)
 }
 
 // Size returns the total number of the nodes in the entire tree.
-func (t *TreeRec) Size() int {
+func (t *Tree) Size() int {
 	return t.count
 }
