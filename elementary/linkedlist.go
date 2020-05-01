ef188270ebae8f2edb22d6a4e8f66f24c4ecb958
Mads Ovesen <m.ovesen@allsaints.com>
2020-05-01 20:02:56 +0100

    refactored linked list

--- elementary/linkedlist.go
+++ elementary/linkedlist.go
@@ -6,81 +6,77 @@ import (
 
 // Various errors a list function can return.
 var (
-	ErrListUnderflow    = errors.New("list underflow")
-	ErrNoElementWithVal = errors.New("no element with value exists in list")
+	ErrDeleteSentinel = errors.New("cannot delete sentinel of list")
 )
 
 // NewLinkedList creates a new instance of a list
 func NewLinkedList() *List {
+	s := &ListElement{
+		value: 0,
+	}
+	s.next = s
+	s.prev = s
+
 	return &List{
-		head: nil,
+		head: s,
 	}
 }
 
 type List struct {
-	head *Element
+	head *ListElement
+}
+
+type ListElement struct {
+	next  *ListElement
+	prev  *ListElement
+	value int
 }
 
 // IsEmpty checks if the list is empty.
 func (l *List) IsEmpty() bool {
-	return l.head == nil
+	return l.head.next == l.head.prev
 }
 
-func (l *List) Append(val int) {
-	e := &Element{
-		next:  nil,
+func (l *List) Insert(val int) {
+	e := &ListElement{
+		next:  l.head.next,
+		prev:  l.head,
 		value: val,
 	}
 
-	h := l.head
-	for h.next != nil {
-		h = h.next
-	}
-
-	h.next = e
+	l.head.next.prev = e
+	l.head.next = e
 }
 
-func (l *List) Prepend(val int) {
-	e := &Element{
-		next:  l.head,
-		value: val,
-	}
-
-	l.head = e
-}
-
-func (l *List) Delete(val int) (*Element, error) {
-	if l.IsEmpty() {
-		return nil, ErrQueueUnderflow
-	}
-
-	if l.head.value == val {
-		l.head = l.head.next
-	} else {
-		h := l.head
-		for h.next != nil {
-			e := h.next
-			if e.value == val {
-				h.next = e.next
-				return e, nil
-			}
-
-			h = h.next
+func (l *List) Search(val int) *ListElement {
+	e := l.head.next
+	for e != l.head {
+		if e.value == val {
+			return e
 		}
+		e = e.next
 	}
 
-	return nil, ErrNoElementWithVal
+	return nil
 }
 
-// Traverse loops through each element in the list.
-func (l *List) Traverse(f func(*Element)) {
-	e := l.head
-	if e != nil {
+func (l *List) Delete(e *ListElement) error {
+	if e == l.head {
+		return ErrDeleteSentinel
+	}
+
+	e.prev.next = e.next
+	e.next.prev = e.prev
+
+	return nil
+}
+
+// Traverse loops through each element in the list until it reaches the
+// sentinel.
+func (l *List) Traverse(f func(*ListElement)) {
+	e := l.head.next
+	for e != l.head {
 		f(e)
-
-		for e.next != nil {
-			e = e.next
-			f(e)
-		}
+		e = e.next
 	}
 }
