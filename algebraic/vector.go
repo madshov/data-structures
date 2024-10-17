974cf6b52d3133709e72c07be269191e6fa5b352
Mads Ovesen <madsovesen@madss-mbp.home>
2024-10-17 19:50:14 +0100

    Fixed a few issues on vector and refactored

--- algebraic/vector.go
+++ algebraic/vector.go
@@ -11,79 +11,64 @@ var (
 	ErrCoordOutOfBounds = errors.New("vector coordinate is greater than dimension")
 	ErrMagZero          = errors.New("vector magnitude cannot be zero")
 	ErrDimsNotEqual     = errors.New("vector dimensions do not match")
+	ErrIndivisbleByZero = errors.New("vector coordinate is not divisble by zero")
 )
+
+// Vector defines a vector structure with a slice of floating point coordinates.
+type vector struct {
+	coords []float64
+}
 
 // NewVector creates a new instance of a Vector with a given dimension and a
 // slice of coordinates, and returns a pointer to it. If the dimension is
 // greater than the number of coordinates, the remaining indexes of the vector
 // will be zero-filled. If the dimension is less, the remaining coordinates will
 // be ignored.
-func NewVector(dim int, coords ...float64) (*Vector, error) {
+func NewVector(dim int, coords ...float64) (*vector, error) {
 	if dim < 0 {
 		return nil, ErrInvalidDim
 	}
 
 	cs := make([]float64, dim)
-	var i int
-	for i < dim {
-		if i < len(coords) {
-			cs[i] = coords[i]
-		} else {
-			cs[i] = 0.0
-		}
-		i++
+	for i := 0; i < dim; i++ {
+		cs[i] = coords[i]
 	}
 
-	return &Vector{
+	return &vector{
 		coords: cs,
 	}, nil
 }
 
 // NewZeroVector creates a new instance of a zero-filled vector with a given
 // dimension.
-func NewZeroVector(dim int) (*Vector, error) {
-	if dim < 0 {
-		return nil, ErrInvalidDim
-	}
-
-	cs := make([]float64, dim)
-	return &Vector{
-		coords: cs,
-	}, nil
+func NewZeroVector(dim int) (*vector, error) {
+	return NewVector(dim)
 }
 
-// NewUnitVector creates a new instance of a unit vector with a given  with a
-// given dimension.
-func NewUnitVector(dim, coord int) (*Vector, error) {
-	if dim < 0 {
-		return nil, ErrInvalidDim
+// NewUnitVector creates a new instance of a unit vector with a given
+// dimension.
+func NewUnitVector(dim, coord int) (*vector, error) {
+	vec, err := NewVector(dim)
+	if err != nil {
+		return nil, err
 	}
 
-	if dim < coord {
+	if len(vec.coords) < coord {
 		return nil, ErrCoordOutOfBounds
 	}
 
-	cs := make([]float64, dim)
-	cs[coord] = 1.0
-
-	return &Vector{
-		coords: cs,
-	}, nil
+	vec.coords[coord] = 1
+	return vec, nil
 }
 
-// Vector defines a vector structure with a slice of floating point coordinates.
-type Vector struct {
-	coords []float64
-}
-
-// Dimension returns the number of coordinates for a given vector.
-func (v *Vector) Dimension() int {
+// Dimension returns the number of coordinates for the vector.
+func (v *vector) Dimension() int {
 	return len(v.coords)
 }
 
-// Magnitude returns the distance from the endpoint to the origin of a given
+// Magnitude returns the distance from the endpoint to the origin for the
 // vector.
-func (v *Vector) Magnitude() float64 {
+func (v *vector) Magnitude() float64 {
 	var l float64
 	for _, c := range v.coords {
 		l += c * c
@@ -96,9 +81,9 @@ func (v *Vector) Magnitude() float64 {
 	return math.Sqrt(l)
 }
 
-// Normalize normalizes, i.e. divides each coodinate with its magnitude, a given
-// vector.
-func (v *Vector) Normalize() error {
+// Normalize normalizes, i.e. divides each coodinate with its magnitude, a for
+// the vector.
+func (v *vector) Normalize() error {
 	mag := v.Magnitude()
 	if mag == 0 {
 		return ErrMagZero
@@ -111,9 +96,9 @@ func (v *Vector) Normalize() error {
 	return nil
 }
 
-// Add adds each coordinate of two given vectors.
-func (v *Vector) Add(w *Vector) *Vector {
-	rv := &Vector{
+// Add adds each coordinate of two vectors.
+func (v *vector) Add(w *vector) *vector {
+	rv := &vector{
 		coords: make([]float64, v.Dimension()),
 	}
 
@@ -124,9 +109,9 @@ func (v *Vector) Add(w *Vector) *Vector {
 	return rv
 }
 
-// Sub substracts each coordinate of two given vectors.
-func (v *Vector) Sub(w *Vector) *Vector {
-	rv := &Vector{
+// Sub substracts each coordinate of two vectors.
+func (v *vector) Sub(w *vector) *vector {
+	rv := &vector{
 		coords: make([]float64, v.Dimension()),
 	}
 
@@ -137,9 +122,9 @@ func (v *Vector) Sub(w *Vector) *Vector {
 	return rv
 }
 
-// Mul multiplies each coordinate of two given vectors.
-func (v *Vector) Mul(w *Vector) *Vector {
-	rv := &Vector{
+// Mul multiplies each coordinate of two vectors.
+func (v *vector) Mul(w *vector) *vector {
+	rv := &vector{
 		coords: make([]float64, v.Dimension()),
 	}
 
@@ -150,21 +135,24 @@ func (v *Vector) Mul(w *Vector) *Vector {
 	return rv
 }
 
-// Div divides each coordinate of two given vectors.
-func (v *Vector) Div(w *Vector) *Vector {
-	rv := &Vector{
+// Div divides each coordinate of two vectors.
+func (v *vector) Div(w *vector) (*vector, error) {
+	rv := &vector{
 		coords: make([]float64, v.Dimension()),
 	}
 
 	for k, c := range v.coords {
+		if w.coords[k] == 0 {
+			return nil, ErrIndivisbleByZero
+		}
 		rv.coords[k] = c / w.coords[k]
 	}
 
-	return rv
+	return rv, nil
 }
 
-// Dot returns the dot product (scalar product) of two given vectors.
-func (v *Vector) Dot(w *Vector) float64 {
+// Dot returns the dot product (scalar product) of two vectors.
+func (v *vector) Dot(w *vector) float64 {
 	var d float64
 	for k, c := range v.coords {
 		d += c * w.coords[k]
@@ -173,9 +161,9 @@ func (v *Vector) Dot(w *Vector) float64 {
 	return d
 }
 
-// Scale scales each coordinate in a given vector with a given scalar value.
-func (v *Vector) Scale(scalar float64) *Vector {
-	rv := &Vector{
+// Scale scales each coordinate in the vector with a given scalar value.
+func (v *vector) Scale(scalar float64) *vector {
+	rv := &vector{
 		coords: make([]float64, v.Dimension()),
 	}
 
@@ -186,6 +174,7 @@ func (v *Vector) Scale(scalar float64) *Vector {
 	return rv
 }
 
-func (v *Vector) GetCoord(coord int) float64 {
+// GetCoord returns a given coordinate for the vector.
+func (v *vector) GetCoord(coord int) float64 {
 	return v.coords[coord]
 }
