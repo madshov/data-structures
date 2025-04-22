1953499807ac87833f05651ad8baa17e4e7dded4
Mads Ovesen <mads.helge.ovesen@gmail.com>
2025-04-22 21:03:40 +0100

    Added tests for NewZeroVector and NewUnitVector

--- algebraic/vector_test.go
+++ algebraic/vector_test.go
@@ -10,31 +10,94 @@ import (
 
 func TestNewVector(t *testing.T) {
 	assert := assert.New(t)
+	tests := map[string]struct {
+		dim    uint
+		coords []float64
+		want   []float64
+	}{
+		"should return a new vector with dimension 3 and set values": {
+			dim:    3,
+			coords: []float64{1, 2, 3},
+			want:   []float64{1, 2, 3},
+		},
+		"should return a new vector with dimension 4 and two set values": {
+			dim:    4,
+			coords: []float64{1, 2},
+			want:   []float64{1, 2, 0, 0},
+		},
+		"should return a new vector with dimension 2 and only two set values": {
+			dim:    2,
+			coords: []float64{1, 2, 3},
+			want:   []float64{1, 2},
+		},
+	}
 
-	t.Run("should return a new vector with dimension 3 and set values", func(t *testing.T) {
-		v := algebraic.NewVector(3, 1, 2, 3)
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			v := algebraic.NewVector(test.dim, test.coords...)
 
-		assert.Len(v, 3)
-		assert.Equal(1.0, v[0])
-		assert.Equal(2.0, v[1])
-		assert.Equal(3.0, v[2])
-	})
+			assert.Len(v, int(test.dim))
+			assert.EqualValues(test.want, v)
+		})
+	}
+}
 
-	t.Run("should return a new vector with dimension 4 and two set values", func(t *testing.T) {
-		v := algebraic.NewVector(4, 1, 2)
+func TestNewZeroVector(t *testing.T) {
+	assert := assert.New(t)
+	tests := map[string]struct {
+		dim  uint
+		want []float64
+	}{
+		"should return a new zero vector with dimension 3": {
+			dim:  3,
+			want: []float64{0, 0, 0},
+		},
+		"should return a new zero vector with dimension 5": {
+			dim:  5,
+			want: []float64{0, 0, 0, 0, 0},
+		},
+	}
 
-		assert.Len(v, 4)
-		assert.Equal(1.0, v[0])
-		assert.Equal(2.0, v[1])
-		assert.Equal(0.0, v[2])
-		assert.Equal(0.0, v[3])
-	})
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			v := algebraic.NewZeroVector(test.dim)
 
-	t.Run("should return a new vector with dimension 2 and only two set values", func(t *testing.T) {
-		v := algebraic.NewVector(2, 1, 2, 3)
+			assert.Len(v, int(test.dim))
+			assert.EqualValues(test.want, v)
+		})
+	}
+}
 
-		assert.Len(v, 2)
-		assert.Equal(1.0, v[0])
-		assert.Equal(2.0, v[1])
-	})
+func TestNewUnitVector(t *testing.T) {
+	assert := assert.New(t)
+	tests := map[string]struct {
+		dim  uint
+		el   uint
+		want []float64
+	}{
+		"should return a new unit vector with dimension 3": {
+			dim:  3,
+			el:   0,
+			want: []float64{1, 0, 0},
+		},
+		"should return a new unit vector with dimension 3 and el 2": {
+			dim:  3,
+			el:   2,
+			want: []float64{0, 0, 1},
+		},
+		"should return a new unit vector with dimension 3 and el 3": {
+			dim:  3,
+			el:   3,
+			want: []float64{0, 0, 0},
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			v := algebraic.NewUnitVector(test.dim, test.el)
+
+			assert.Len(v, int(test.dim))
+			assert.EqualValues(test.want, v)
+		})
+	}
 }
